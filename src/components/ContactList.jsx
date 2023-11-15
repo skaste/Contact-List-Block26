88ae3c77cda47a90993743bed7aa68eb5c1959a1
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-15 14:27:55 -0600

    created the return for ContactRow function and imported the ContactRow function to ContactList.jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import ContactRow from "./ContactRow";
 function ContactList() {
 
 // const [contacts, setContacts] = useState(dummyContacts);
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,6 +1,12 @@
 
 
-function ContactRow(){
-
+function ContactRow({contact}){
+  return(
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
 }
 export default ContactRow
