4009cf9163b019ba5bd5228da7c329a9871fa495
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-15 14:08:49 -0600

    created a Contact list component in App.jsx and tested

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function ContactList() {
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        {
+          //Map over data here
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default ContactList
