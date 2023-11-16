e0db8690175d6f7167307ba5fe7f9a062ed4943f
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-15 21:15:30 -0600

    added useState for selectedContactId and imported SelectedContact to App.jsx from SelectedContact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
 import ContactList from './components/ContactList';
 import { useState } from 'react';
+import SelectedContact from './components/SelectedContact';
 
 
 
 
 function App(){
-
+const [selectedContactId, setSelectedContactId] = useState(null);
   return(
     <>
+    {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ):(
     <ContactList/>
-   
+    )}
     </>
   );
-
 }
 export default App
